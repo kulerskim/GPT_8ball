01caf8c99590c7603ab96bfda98d228e5886a52c
Mariusz Kulerski <kuler@cersoft.pl>
2023-04-12 23:43:54 +0200

    initial commit

---
+++ gpt_8ball.py
@@ -0,0 +1,8 @@
+def main():
+    print("Welcome to GPT-8Ball!")
+    while True:
+        user_input = input(">> ")
+        print("GPT-8Ball: ", user_input)
+
+if __name__ == "__main__":
+    main()
