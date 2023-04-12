684340147fa8c6a4914689c6c1bbb8fd59fb7d90
Mariusz Kulerski <kuler@cersoft.pl>
2023-04-13 00:55:05 +0200

    add basic exception handling

--- gpt_8ball.py
+++ gpt_8ball.py
@@ -4,17 +4,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def call_gpt4_api(prompt):
-    openai.api_key = os.environ["GPT_8BALL_API_KEY"]
+openai.api_key = os.environ["GPT_8BALL_API_KEY"]
 
-    result = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}])
-    message = result["choices"][0]["message"]
-    return message["content"]
+def call_gpt4_api(prompt):
+    try:
+        result = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}])
+        message = result["choices"][0]["message"]
+        return message["content"]
+    except openai.error.OpenAIError:
+        return "Oops, there was an error processing your request. Please try again."
 
 def main():
     print("Welcome to GPT-8Ball!")
     while True:
-        user_input = input(">> ")
+        try:
+            user_input = input(">> ")
+        except KeyboardInterrupt:
+            print("\nGoodbye!")
+            break
         response = call_gpt4_api(user_input)
         print("GPT-8Ball: ", response)
 
