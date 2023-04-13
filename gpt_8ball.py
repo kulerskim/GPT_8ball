d3fe2a542030c458d659691478f84f258de99684
Mariusz Kulerski <kuler@cersoft.pl>
2023-04-14 00:23:04 +0200

    keep conversation context when talking to API

--- gpt_8ball.py
+++ gpt_8ball.py
@@ -2,19 +2,24 @@ import os
 import openai
 from dotenv import load_dotenv
 
+from conversation_history import ConversationHistory
+
 load_dotenv()
 
 class GPT8Ball:
     def __init__(self, api_key):
         self.api_key = api_key
         self.model = "gpt-3.5-turbo"
+        self.conversation_history = ConversationHistory()
         self.openai = openai
         self.openai.api_key = self.api_key
 
     def call_chat_completion(self, prompt):
         try:
-            result = self.openai.ChatCompletion.create(model=self.model, messages=[{"role": "user", "content": prompt}])
+            self.conversation_history.add({"role": "user", "content": prompt})
+            result = self.openai.ChatCompletion.create(model=self.model, messages=self.conversation_history.get().copy())
             message = result["choices"][0]["message"]
+            self.conversation_history.add(message)
             return message["content"]
         except self.openai.error.OpenAIError:
             return "Oops, there was an error processing your request. Please try again."
--- tests/test_gpt_8ball.py
+++ tests/test_gpt_8ball.py
@@ -9,6 +9,34 @@ def test_call_chat_completion_creates_valid_api_call():
 
     gpt_8ball.call_chat_completion("hello")
     
-    openai_mock.ChatCompletion.create.assert_any_call(
+    openai_mock.ChatCompletion.create.called_with(
         model=gpt_8ball.model, messages=[{"role": "user", "content": "hello"}]
     )
+
+def test_call_chat_completion_keeps_conversation_history():
+    gpt_8ball = GPT8Ball("")
+
+    openai_mock = MagicMock()
+    openai_mock.ChatCompletion.create.return_value = {
+        "choices": [{
+            "message": {
+                "role": "assistant",
+                "content": "Hi there, I'm your assistant how can I help you?"
+            }
+        }]
+    }
+
+    gpt_8ball.openai = openai_mock
+
+    gpt_8ball.call_chat_completion("hello")
+    gpt_8ball.call_chat_completion("bye bye")
+
+    openai_mock.ChatCompletion.create.assert_called_with(
+        model=gpt_8ball.model, messages=[
+            {"role": "user", "content": "hello"},
+            {"role": "assistant", "content": "Hi there, I'm your assistant how can I help you?"},
+            {"role": "user", "content": "bye bye"}
+        ]
+    )
+
+
