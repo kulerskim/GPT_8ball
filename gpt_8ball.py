cf68af86d083805db479a569a818f033d1a7c1fb
Mariusz Kulerski <kuler@cersoft.pl>
2023-04-13 22:42:48 +0200

    refactor to use OOP

--- gpt_8ball.py
+++ gpt_8ball.py
@@ -4,26 +4,31 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-openai.api_key = os.environ["GPT_8BALL_API_KEY"]
+class GPT8Ball:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        openai.api_key = self.api_key
 
-def call_gpt4_api(prompt):
-    try:
-        result = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}])
-        message = result["choices"][0]["message"]
-        return message["content"]
-    except openai.error.OpenAIError:
-        return "Oops, there was an error processing your request. Please try again."
-
-def main():
-    print("Welcome to GPT-8Ball! Press Ctrl+C to exit.")
-    while True:
+    def call_chat_completion(self, prompt):
         try:
-            user_input = input(">> ")
-        except KeyboardInterrupt:
-            print("\nGoodbye!")
-            break
-        response = call_gpt4_api(user_input)
-        print("GPT-8Ball: ", response)
+            result = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}])
+            message = result["choices"][0]["message"]
+            return message["content"]
+        except openai.error.OpenAIError:
+            return "Oops, there was an error processing your request. Please try again."
+
+    def run(self):
+        print("Welcome to GPT-8Ball! Press Ctrl+C to exit.")
+        while True:
+            try:
+                user_input = input(">> ")
+            except KeyboardInterrupt:
+                print("\nGoodbye!")
+                break
+            response = self.call_chat_completion(user_input)
+            print("GPT-8Ball: ", response)
 
 if __name__ == "__main__":
-    main()
+    api_key = os.environ["GPT_8BALL_API_KEY"]
+    gpt_ball = GPT8Ball(api_key)
+    gpt_ball.run()
